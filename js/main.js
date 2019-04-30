60072fb1325193bef880004d8a1c06900eab25fd
jrmarqueshd <gilmar_junior@hotmail.com.br>
2019-04-30 08:06:30 -0300

    add other function to string

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 window.addEventListener("load", ()=>{
 
     let $keyboard = document.querySelectorAll(".key");
-    $input = document.querySelector("input");
-    $keyboard.forEach((e, key)=>{
+        $input = document.querySelector("input");
+        $keyboard.forEach((e, key)=>{
         let i = 0;
         let conc = [];
+        let palavra = [];
         e.addEventListener("click", ()=>{
-           res = conc[i].concat(e.value);
-                console.log(res);
-           i++;
+        //    res = conc[i].concat(e.value);
+        //         console.log(res);
+        //    i++;
+
+            palavra = palavra + e.value;
+            
+            $input.value = palavra;
+
+            i++;
         })
     });
 });
