52bb1cbadda19afdaaae22ea1b4b6904c652cc22
junior <jr.info@hotmail.com.br>
2019-05-01 21:42:31 -0300

    functionally key on my input

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 window.addEventListener("load", ()=>{
 
+    // Chamando o input do formulário
+    let $input = document.querySelector("input");
+
+    // Chamando cada tecla do teclado virtual
+    // * O value do elemento já foi setado diretamente no próprio HTML
     let $keyboard = document.querySelectorAll(".key");
-        $input = document.querySelector("input");
-        $keyboard.forEach((e, key)=>{
-        let i = 0;
-        let conc = [];
-        let palavra = [];
+
+    // Percorrendo todos os elementos do meu teclado para te-los carregados na window
+    $keyboard.forEach((e)=>{
+
+        // Adicionando o evento de clique para o elemento atual que eu clicar
         e.addEventListener("click", ()=>{
-        //    res = conc[i].concat(e.value);
-        //         console.log(res);
-        //    i++;
-
-            palavra = palavra + e.value;
             
-            $input.value = palavra;
+            // regra utilizada:
+            // * $input.value, pego o valor atual do input do formulário
+            // ** $input.value = $input.value + e.value, ordeno que o valor atual do input receberá
+            // ** o valor atual do YouTube mais o valor do elemento que eu cliquei.
+            // *** ( $input.value = ... ) Dessa forma eu já imprimo o valor direto no .value do meu elemento
+            // **** Fonte principal de pesquisa ( https://www.youtube.com/watch?v=dbVHI0x3lj0&t=336s ).
 
-            i++;
+            $input.value = $input.value + e.value;
         })
     });
+
 });
